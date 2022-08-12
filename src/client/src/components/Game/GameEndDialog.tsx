48fa2ca310dff2defe1c5f394b60cd6ed474dab8
Aldo Camacho <aldo@aldocamacho.com>
2022-08-12 14:23:11 -0700

    add dialog styles

--- src/client/src/components/Game/GameEndDialog.tsx
+++ src/client/src/components/Game/GameEndDialog.tsx
@@ -1,19 +1,46 @@
-import { Dialog, Box, Button, Typography } from "@mui/material";
+import { Dialog, Box, Typography } from "@mui/material";
 
 import Cross from "../Icons/Cross";
 import Circle from "../Icons/Circle";
+import BoardButton from "../BoardButton";
 import { Player } from "../../utils/types";
+import { darkTheme } from "../../utils/theme";
 
 const styles = {
-  dialog: {
-    // width: "100vw",
+  dialog: {},
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 2,
   },
   paper: {
     maxWidth: "100vw",
     width: "100%",
     margin: 0,
+    padding: "40px 10px",
     border: "unset",
   },
+  winner: {
+    color: darkTheme.palette.colors.accent,
+    fontSize: 13,
+    fontWeight: "bold",
+  },
+  message: {
+    display: "flex",
+    alignItems: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+    "& svg": {
+      height: 45,
+      marginRight: 1,
+    },
+  },
+  buttons: {
+    display: "flex",
+    gap: 1
+  }
 };
 
 interface Props {
@@ -24,6 +51,16 @@ interface Props {
 }
 
 const GameEndDialog = ({ onClose, open, activePlayer, winner }: Props) => {
+  const nextButtonColor =
+    activePlayer === "x"
+      ? darkTheme.palette.colors.circle
+      : darkTheme.palette.colors.cross;
+
+  const messageColor =
+    activePlayer === "x"
+      ? darkTheme.palette.colors.cross
+      : darkTheme.palette.colors.circle;
+
   return (
     <Dialog
       onClose={onClose}
@@ -36,14 +73,25 @@ const GameEndDialog = ({ onClose, open, activePlayer, winner }: Props) => {
         elevation: 0,
       }}
     >
-      <Typography>{winner ? "YOU WON!" : "YOU LOSE :("}</Typography>
-      <Box sx={{ "& svg": { height: 20 } }}>
-        {activePlayer === "x" ? <Cross /> : <Circle />}
-        TAKES THE ROUND
-      </Box>
-      <Box>
-        <Button onClick={onClose}>QUIT</Button>
-        <Button onClick={() => console.log("next round")}>NEXT ROUND</Button>
+      <Box sx={styles.container}>
+        <Typography sx={styles.winner}>
+          {winner ? "YOU WON!" : "YOU LOSE :("}
+        </Typography>
+
+        <Box sx={{ ...styles.message, color: messageColor }}>
+          {activePlayer === "x" ? <Cross /> : <Circle />}
+          TAKES THE ROUND
+        </Box>
+
+        <Box sx={styles.buttons}>
+          <BoardButton onClick={onClose}>QUIT</BoardButton>
+          <BoardButton
+            onClick={() => console.log("next round")}
+            color={nextButtonColor}
+          >
+            NEXT ROUND
+          </BoardButton>
+        </Box>
       </Box>
     </Dialog>
   );
