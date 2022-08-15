ad46a9c2968fe8e18815ad318d6c62e7fd601115
Aldo Camacho <aldo@aldocamacho.com>
2022-08-15 11:56:22 -0700

    add basic server

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,10 @@
 import express from "express";
+
 const app = express();
-const port = 3001;
 
 app.get("/", (req, res) => {
   console.log("request at", req.originalUrl);
   res.json({ foo: "bar" });
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+export default app;
