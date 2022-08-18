b78f78f239280bfdef5aaabc7d6a6c8e6034b31a
Aldo Camacho <aldo@aldocamacho.com>
2022-08-18 12:03:59 -0700

    specify node version in package.json

--- src/server/utils/config.ts
+++ src/server/utils/config.ts
@@ -1,8 +1,7 @@
 import "dotenv/config";
 
 export const PORT = process.env.PORT || 3001;
-export const REDIS_URL =
-  process.env.REDIS_URL || process.env.REDISCLOUD_URL || "";
+export const REDIS_URL = process.env.REDISCLOUD_URL || "";
 export const SECRET = process.env.SECRET || "secret";
 export const ANON_USER_TTL = process.env.ANON_USER_TTL || 3600; // time in seconds
 export const GAME_TTL = process.env.GAME_TTL || 3600;
