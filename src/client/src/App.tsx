046a7643f29d07407a546a9ddc03db349a51771c
Aldo Camacho <aldo@aldocamacho.com>
2022-08-19 10:23:27 -0700

    add header and login

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
 import GameContainer from "./components/GameContainer";
+import Header from "./components/Header";
 import { darkTheme } from "./utils/theme";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
+      <Header />
       <GameContainer />
     </ThemeProvider>
   );
