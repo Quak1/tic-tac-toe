353c58002bbc72848399bae3dcda8ee6a0333c43
Aldo Camacho <aldo@aldocamacho.com>
2022-08-19 12:53:07 -0700

    update variable name for redis connection url

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 export const PORT = process.env.PORT || 3001;
-export const REDIS_URL = process.env.REDIS_URL || "";
+export const REDIS_URL = process.env.REDISCLOUD_URL || "";
 export const SECRET = process.env.SECRET || "secret";
 export const ANON_USER_TIMEOUT = process.env.ANON_USER_TIMEOUT || 3600; // time in seconds
 export const GAME_TIMEOUT = process.env.GAME_TIMEOUT || 3600;
