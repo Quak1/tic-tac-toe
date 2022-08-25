dd5945deac56ea3cd815019e53f0fcd96e4bb6c3
Aldo Camacho <aldo@aldocamacho.com>
2022-08-24 17:02:01 -0700

    add new challenge options to header

--- src/client/src/components/Header/index.tsx
+++ src/client/src/components/Header/index.tsx
@@ -4,6 +4,7 @@ import { Typography } from "@mui/material";
 import TempLoginForm from "./TempLogin";
 import ChallengePlayerForm from "./ChallengePlayer";
 import AuthContext from "../../context/AuthProvider";
+import WaitGameButton from "./WaitGameButton";
 
 const Header = () => {
   const authContext = useContext(AuthContext);
@@ -12,6 +13,7 @@ const Header = () => {
     <>
       <Typography>{`${authContext.user.username}#${authContext.user.id}`}</Typography>
       <ChallengePlayerForm />
+      <WaitGameButton />
     </>
   ) : (
     <TempLoginForm />
