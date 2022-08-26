6c70e77999b8333bc4d4b687e90f97d11137c175
Aldo Camacho <aldo@aldocamacho.com>
2022-08-26 14:20:11 -0700

    update game status after match is accepted

--- src/client/src/components/Header/ChallengePlayer.tsx
+++ src/client/src/components/Header/ChallengePlayer.tsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
+
+import GameContext from "../../context/GameStatusProvider";
 import ToggleableSingleInputForm from "../Forms/ToggleableSingleInputForm";
 import { FormData } from "../Forms/SingleInputForm";
 import axios from "../../api/axios";
 
 const ChallengePlayerForm = () => {
+  const gameContext = useContext(GameContext);
+
   const challengePlayer = async (data: FormData) => {
     try {
       const res = await axios.get(`/game/challenge/${data.field}`);
       console.log(res);
+      gameContext.setGameStatus(res.data);
     } catch (e) {
       // TODO notify user
       console.log(e);
